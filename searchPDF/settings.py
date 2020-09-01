d36c0978df13e291770a9675566f13f056fbf38d
MalaySaxena <44743128+MalaySaxena@users.noreply.github.com>
2020-09-01 17:23:35 +0530

    update ALLOWED_HOST

--- searchPDF/settings.py
+++ searchPDF/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e140=y9al5yg@ade5(m)xv+zpzd=jp
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thawing-ocean-66765.herokuapp.com','127.0.0.1']
 
 
 # Application definition
